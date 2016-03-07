3fba88933d97aa1ad42a5f5fee7e4542b781c603
carloscasen <carloscasen677@gmail.com>
2016-03-07 18:43:21 +0100

    workspace

---
+++ Workspace/pris2/src/org/pris2/polinomio/polinomio.java
@@ -0,0 +1,11 @@
+package org.pris2.polinomio;
+
+public class polinomio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola gorda");
+
+	}
+
+}
